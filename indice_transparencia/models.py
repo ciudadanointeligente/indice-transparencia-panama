b9007486766c76dd48c8a6fadcdd958056451343
Felipe Álvarez <luisfelipealvarez@gmail.com>
2018-12-11 14:48:48 -0200

    Creando el primer modelo

--- indice_transparencia/models.py
+++ indice_transparencia/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+TYPES_OF_PERSON = (('parlamentario', 'Parlamentario'), ('candidato', 'Candidato'), )
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    specific_type = models.CharField(max_length=255,
+                                     choices=TYPES_OF_PERSON)
--- indice_transparencia/tests/test_models.py
+++ indice_transparencia/tests/test_models.py
@@ -1,4 +1,14 @@
+import pytest
+from indice_transparencia.models import Person
+
+pytestmark = pytest.mark.django_db
+
+@pytest.mark.django_db
 class TestPerrito(object):
+    pytestmark = pytest.mark.django_db
 
     def test_perrito(self):
-        assert 1 == 2
+        p = Person.objects.create(name=u'Fiera',
+                                  specific_type='parlamentario')
+        assert p.name == 'Fiera'
+        assert p.specific_type == 'parlamentario'
