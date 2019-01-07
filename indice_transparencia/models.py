bd9ecf95ee183004c5164e4554b69e98f146667b
Felipe Álvarez <luisfelipealvarez@gmail.com>
2019-01-07 19:05:57 -0300

    Arreglando los campos y otras cosas

--- indice_transparencia/models.py
+++ indice_transparencia/models.py
@@ -1,56 +1,106 @@
 from django.db import models
+from autoslug import AutoSlugField
+
+
+class Party(models.Model):
+    name = models.CharField(max_length=255, verbose_name=u"Nombre")
+    initials = models.CharField(max_length=255, verbose_name=u"Iniciales")
+    slug = AutoSlugField(populate_from='name', null=True)
+
+
+class EducationalRecord(models.Model):
+    name = models.CharField(max_length=255, verbose_name=u"Nombre Programa")
+    institution = models.CharField(max_length=255, verbose_name=u"Institución")
+    start = models.CharField(max_length=255, verbose_name=u"Fecha de ingreso")
+    end = models.CharField(max_length=255, verbose_name=u"Fecha de término")
+    person = models.ForeignKey('Person', on_delete=models.CASCADE, related_name="educational_records", null=True)
+
+
+class WorkRecord(models.Model):
+    name = models.CharField(max_length=255, verbose_name=u"Cargo")
+    institution = models.CharField(max_length=255, verbose_name=u"Institución")
+    start = models.CharField(max_length=255, verbose_name=u"Fecha de ingreso")
+    end = models.CharField(max_length=255, verbose_name=u"Fecha de término")
+    person = models.ForeignKey('Person', on_delete=models.CASCADE, related_name="work_records", null=True)
+    
+
+class JudiciaryProcessRecord(models.Model):
+    number = models.CharField(max_length=255, verbose_name=u"Número")
+    date = models.DateField(max_length=255, verbose_name=u"Fecha")
+    kind = models.CharField(max_length=255, verbose_name=u"Tipo")
+    result = models.TextField(max_length=255, verbose_name=u"Fallo")
+    person = models.ForeignKey('Person', on_delete=models.CASCADE, related_name="judiciary_records", null=True)
+
+
+class Benefit(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nombre del beneficio')
+
 
 TYPES_OF_PERSON = (('parlamentario', 'Parlamentario'), ('candidato', 'Candidato'), )
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=255, verbose_name=u"Nombre")
+    name = models.CharField(max_length=255, verbose_name=u"Nombre Completo")
     specific_type = models.CharField(max_length=255,
                                      choices=TYPES_OF_PERSON,
                                      verbose_name=u"Tipo de persona",
                                      help_text=u"Parlamentario o candidato")
-    gender = models.CharField(max_length=255, null=True)
     birth_date = models.DateField(verbose_name=u"Fecha de nacimiento", null=True)
-    email = models.EmailField(verbose_name=u"Correo electrónico", null=True)
-    web = models.URLField(verbose_name=u"Página web personal", null=True)
-    party = models.CharField(max_length=255, verbose_name=u"Partido", null=True)
-    circuit = models.CharField(max_length=255, verbose_name=u"Circuito", null=True)
-    period = models.CharField(max_length=255, verbose_name=u"Periodo", null=True)
+    email = models.EmailField(verbose_name=u"Correo electrónico de contacto", null=True)
+    web = models.URLField(max_length=512, verbose_name=u"Link al sitio web personal (o cuenta oficial en redes sociales)", null=True)
+    declared_intention_to_transparent = models.BooleanField(default=False, verbose_name=u"¿Desea Ud. transparentar su información política general?")
+    party = models.ForeignKey(Party, null=True, on_delete=models.SET_NULL, related_name="persons")
+    circuit = models.CharField(max_length=255, verbose_name=u"Circuito al que representa o busca representar", null=True)
+    period = models.CharField(max_length=255, verbose_name=u"¿En qué período legislativo se encuentra actualmente?", null=True)
+    previous_parties = models.ManyToManyField(Party, related_name="ex_members", verbose_name=u"¿Ha pertenecido ud. a otros partidos o movimientos políticos?")
     reelection = models.BooleanField(default=False, verbose_name=u"¿Va a reelección?", null=True)
-    cv_educ = models.BooleanField(default=False, verbose_name=u"¿Publica su formación académica?", null=True)
-    cv_educ_detail = models.TextField(verbose_name=u"Detalle de su formación académica", null=True)
-    cv_experpro = models.BooleanField(default=False, verbose_name=u"¿Publica su experiencia profesional?", null=True)
-    cv_experpro_detail = models.URLField(verbose_name=u"Link a CV con detalles.", null=True)
-    cv_experpol = models.BooleanField(default=False, verbose_name=u"¿Publica su experiencia en otros cargos políticos?", null=True)
-    cv_experpol_detail =  models.TextField(verbose_name=u"Texto con resumen de su experiencia política previa", null=True)
-    cv_transfu = models.IntegerField(verbose_name=u"Transfuguismo",
-                                     help_text=u"número de veces que ha cambiado de afiliación política (partido o movimientos)",
-                                     default=0)
-    wor_attendance = models.FloatField(verbose_name=u"Trabajo legislativo / Asistencia",
-                                       help_text=u"porcentaje de asistencia a la Asamblea en el período",
-                                     default=0)
-    wor_proposals = models.IntegerField(verbose_name=u"Trabajo legislativo / Propuestas",
-                                       help_text=u"número de propuestas (de discusión, proyectos de ley, etc) realizadas durante el período",
-                                     default=0)
-    wor_vote = models.IntegerField(verbose_name=u"Trabajo legislativo / Votaciones",
-                                   help_text=u"número de participaciones en votaciones durante el último período",
-                                     default=0)
-    eth_080 = models.BooleanField(default=False, verbose_name=u"Ética presupuestaria / planilla 080",
-                                   help_text=u"¿publica planilla 080?")
-    eth_080_link = models.URLField(verbose_name=u"Ética presupuestaria / planilla 080",
+
+    extra_education = models.TextField(max_length=512,
+                                       null=True,
+                                       verbose_name=u"¿Desea transparentar alguna otra experiencia relevante de formación? Puede escribirlas a continuación")
+    intention_to_transparent_work_plan = models.BooleanField(default=False, verbose_name=u"¿Desea Ud. transparentar su plan de trabajo de diputado(a) o candidato(a)?")
+
+    work_plan_link = models.URLField(null=True, max_length=255, verbose_name=u"Si respondió 'sí', indique en qué link se puede acceder a su programa de trabajo")
+    work_plan_doc = models.FileField(upload_to='work_plans/%Y/%m/%d/',
+                                     null=True,
+                                     verbose_name=u"Si respondió 'sí' pero no tiene su plan de trabajo online, acá tiene la posibilidad de adjuntar el archivo")
+    intention_to_transparent_patrimony = models.BooleanField(default=False, null=True, verbose_name=u"¿Desea Ud. transparentar sus declaraciones de Patrimonio e Intereses")
+    patrimony_link = models.URLField(null=True,
+                                     verbose_name=u"Si respondió 'sí' por favor indique a continuación el link para acceder a su declaración de patrimonio")
+    patrimony_doc = models.FileField(upload_to='patrimony/%Y/%m/%d/',
+                                     null=True,
+                                     verbose_name=u"Si respondió 'sí' pero no tiene su declaración de patrimonio publicada online, acá tiene la oportunidad de adjuntar el archivo")
+
+    existing_interests_declaration = models.BooleanField(default=False, null=True, verbose_name=u"¿Cuenta ud. con una declaración de intereses actualizada?")
+    interests_link = models.CharField(max_length=255, null=True,
+                                      verbose_name=u"Si respondió 'sí' por favor indique a continuación el link para acceder a su declaración de Intereses")
+    interests_doc = models.FileField(upload_to='patrimony/%Y/%m/%d/',
+                                     null=True,
+                                     verbose_name=u"Si respondió 'sí' pero no tiene su declaración de intereses publicada online, acá tiene la oportunidad de adjuntar el archivo")
+    judiciary_declaration = models.BooleanField(default=False,
+                                                verbose_name=u"¿Desea Ud. transparentar información sobre los procesos judiciales en los que ha estado involucrado(a)?")
+
+    extra_judiciary_declaration = models.TextField(max_length=255,null=True, verbose_name=u"¿Se ha visto involucrado en más procesos judiciales?")
+    judiciary_link = models.URLField(null=True, verbose_name=u"Si respondió 'sí', por favor indique dónde se puede acceder a esta información (facilite un link u otro recurso)")
+    judiciary_description = models.TextField(null=True, verbose_name=u"¿Desea agregar comentarios o notas aclaratorias sobre uno o más de los procesos judiciales declarados?")
+    reelection = models.BooleanField(default=False, verbose_name=u"¿Eres actualmente diputado/a?")
+    benefits = models.ManyToManyField(Benefit, null=True)
+
+    benefits_link = models.CharField(max_length=512,
+                                     verbose_name=u"Por favor, indique dónde es posible acceder al detalle sobre los valores asociados al uso de beneficios (se le sugiere indicar un link donde esté publicado el detalle)",
+                                     null=True)
+
+    eth_080_link = models.URLField(verbose_name=u"Indique en qué link es posible acceder al detalle de su planilla 080",
                                    help_text=u"Link a la planilla 080", null=True)
-    eth_172 = models.BooleanField(default=False)
-    eth_172_link = models.URLField(verbose_name=u"Ética presupuestaria / planilla 172",
+    eth_172_link = models.URLField(verbose_name=u"Indique en qué link es posible acceder al detalle de su planilla 172",
                                    help_text=u"Link a la planilla 172", null=True)
-    eth_trav_N = models.IntegerField(verbose_name=u"Ética presupuestaria / Número de viajes",
-                                   help_text=u"Número de viajes realizados por concepto de beneficios.",
-                                     default=0)
-    eth_trav_exp = models.BigIntegerField(verbose_name=u"Ética presupuestaria / Dinero gastado en viajes",
-                                   help_text=u"Monto gastado en viáticos en viajes realizados por concepto de beneficios.",
-                                     default=0)
-    eth_trav_link = models.URLField(verbose_name=u"Link al documento con el detalle de número y viáticos en viajes", null=True)
-    eth_exo = models.BigIntegerField(verbose_name=u"Ética presupuestaria / Exoneración",
-                               help_text=u"monto total de las exoneraciones de vehículos por concepto de beneficios",
-                                     default=0)
-    eth_exo_link = models.URLField(verbose_name=u"Ética presupuestaria / Link a Exoneración",
-                               help_text=u"link al documento con el detalle de los montos por exoneración de vehículos", null=True)
+    eth_080_doc = models.FileField(upload_to='patrimony/%Y/%m/%d/',
+                                  verbose_name=u"Si su planilla 080 no se encuentra publicada online, puede subir el archivo a continuación",
+                                  help_text=u"Link a la planilla 080", null=True)
+    eth_172_doc = models.FileField(upload_to='patrimony/%Y/%m/%d/',
+                                  verbose_name=u"Si su planilla 172 no se encuentra publicada online, puede subir el archivo a continuación",
+                                  help_text=u"Link a la planilla 172", null=True)
+
+
+
+
--- indice_transparencia/tests/test_models.py
+++ indice_transparencia/tests/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from indice_transparencia.models import Person
+from indice_transparencia.models import Person, Party, JudiciaryProcessRecord, WorkRecord, EducationalRecord, Benefit
 
 pytestmark = pytest.mark.django_db
 
@@ -7,36 +7,58 @@ pytestmark = pytest.mark.django_db
 class TestPerrito(object):
     pytestmark = pytest.mark.django_db
 
-    def test_perrito(self):
+    def test_instanciate_person(self):
         p = Person.objects.create(name=u'Fiera',
                                   specific_type='parlamentario')
         assert p.name == 'Fiera'
         assert p.specific_type == 'parlamentario'
         assert hasattr(p, 'name')
-        assert hasattr(p, 'gender')
-        assert hasattr(p, 'birth_date')
         assert hasattr(p, 'email')
         assert hasattr(p, 'web')
         assert hasattr(p, 'party')
         assert hasattr(p, 'circuit')
         assert hasattr(p, 'period')
         assert hasattr(p, 'reelection')
-        assert hasattr(p, 'cv_educ')
-        assert hasattr(p, 'cv_educ_detail')
-        assert hasattr(p, 'cv_experpro')
-        assert hasattr(p, 'cv_experpro_detail')
-        assert hasattr(p, 'cv_experpol')
-        assert hasattr(p, 'cv_experpol_detail')
-        assert hasattr(p, 'cv_transfu')
-        assert hasattr(p, 'wor_attendance')
-        assert hasattr(p, 'wor_proposals')
-        assert hasattr(p, 'wor_vote')
-        assert hasattr(p, 'eth_080')
-        assert hasattr(p, 'eth_080_link')
-        assert hasattr(p, 'eth_172')
-        assert hasattr(p, 'eth_172_link')
-        assert hasattr(p, 'eth_trav_N')
-        assert hasattr(p, 'eth_trav_exp')
-        assert hasattr(p, 'eth_trav_link')
-        assert hasattr(p, 'eth_exo')
-        assert hasattr(p, 'eth_exo_link')
+
+    def test_instanciate_partido(self):
+        p = Party.objects.create(name=u'Partido Feroz',
+                                 initials='PF')
+        assert p.name == 'Partido Feroz'
+        assert p.initials == 'PF'
+        assert bool(p.slug)
+
+    def test_instanciate_judiciary_record(self):
+        p = Person.objects.create(name=u'Fiera',
+                                  specific_type='parlamentario')
+        record = JudiciaryProcessRecord(number='1-2', date='13/04/2015', kind='judicial', person=p)
+        assert record
+
+    def test_instanciate_judiciary_record(self):
+        p = Person.objects.create(name=u'Fiera',
+                                  specific_type='parlamentario')
+        record = JudiciaryProcessRecord(number='1-2', date='13/04/2015', kind='judicial', person=p)
+        assert record
+        assert record.date
+
+    def test_instanciate_work_record(self):
+        p = Person.objects.create(name=u'Fiera',
+                                  specific_type='parlamentario')
+        record = WorkRecord(name='Junior de la empresa', institution='FCI', start='04/07/2011', end='31/01/2018', person=p)
+        assert record
+        assert record.start
+        assert record.end
+
+    def test_instanciate_educational_record(self):
+        p = Person.objects.create(name=u'Fiera',
+                                  specific_type='parlamentario')
+        record = EducationalRecord(name='Junior de la empresa', institution='FCI', start='04/07/2011', end='31/01/2018', person=p)
+        assert record
+        assert record.start
+        assert record.end
+
+    def test_instanciate_benefit(self):
+        p = Person.objects.create(name=u'Fiera',
+                                  specific_type='parlamentario')
+        b = Benefit.objects.create(name="El beneficio")
+        p.benefits.add(b)
+        assert p.benefits.count() > 0
