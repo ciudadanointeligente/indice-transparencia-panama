b6dec1a6ec4b8328c24cb4c235c06a6a9fa748a4
Felipe Álvarez <luisfelipealvarez@gmail.com>
2019-01-08 12:15:25 -0300

    Agregando la administración

--- indice_transparencia/admin.py
+++ indice_transparencia/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from indice_transparencia.models import Person, Party, JudiciaryProcessRecord, WorkRecord, EducationalRecord, Benefit
 
-# Register your models here.
+
+class PartyAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Party, PartyAdmin)
+
+
+class BenefitAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Benefit, BenefitAdmin)
+
+class WorkRecordInline(admin.TabularInline):
+    model = WorkRecord
+
+class JudiciaryProcessRecordInline(admin.TabularInline):
+    model = JudiciaryProcessRecord
+
+
+class EducationalRecordInline(admin.TabularInline):
+    model = EducationalRecord
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [
+        EducationalRecordInline,
+        WorkRecordInline,
+        JudiciaryProcessRecordInline,
+
+    ]
+admin.site.register(Person, PersonAdmin)
