5f75b0a80ad7ad4f83cbbfc1e1355d307ae9b79f
Jordi Bari <jbari@votainteligente.cl>
2019-01-15 14:21:44 +0000

    wip edu records inline formset

--- indice_transparencia/views.py
+++ indice_transparencia/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.edit import UpdateView
 from django.views.generic.detail import DetailView
-from indice_transparencia.models import Person, Contact
-from indice_transparencia.forms import PersonForm
+from indice_transparencia.models import Person, Contact, EducationalRecord
+from indice_transparencia.forms import PersonForm, EducationalRecordInlineFormset
 from django.template.response import TemplateResponse
 
 
@@ -23,6 +23,9 @@ class PersonUpdateView(UpdateView):
     def get_context_data(self, *args, **kwargs):
         context = super(PersonUpdateView, self).get_context_data(*args, **kwargs)
         context['contact'] = Contact.objects.get(identifier=self.identifier)
+        context['formset'] = EducationalRecordInlineFormset(
+            queryset=self.person.educational_records.all()
+        )
         return context
 
 class CandidateProfileView(DetailView):
