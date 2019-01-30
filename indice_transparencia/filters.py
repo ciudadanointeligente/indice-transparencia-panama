1cc44854a7cc74f55224b14fb9664ccfac32bea7
Natalia Matayoshi <natalia@ciudadaniai.org>
2019-01-30 11:37:07 -0300

    Update filters.py

--- indice_transparencia/filters.py
+++ indice_transparencia/filters.py
@@ -2,21 +2,20 @@ import django_filters
 from indice_transparencia.models import Person
 
 class PersonFilter(django_filters.FilterSet):
-    
+
     CHOICES = (
-        ('asc','de menor a mayor'), 
+        ('asc','de menor a mayor'),
         ('desc','de mayor a menor')
         )
-    
-    ordering = django_filters.ChoiceFilter(label='Puntaje', choices=CHOICES, method='filter_by_order')
-    
+
+    ordering = django_filters.ChoiceFilter(label='Filtrar según puntaje en índice de transparencia', choices=CHOICES, method='filter_by_order')
+
     class Meta:
         model = Person
         fields = {'circuit', 'party', 'is_deputy'}
-        
+
     def filter_by_order(self, queryset, name, value):
         expression = '-position_in_ranking' if value == 'desc' else 'position_in_ranking'
-        
+
         return queryset.order_by(expression)
         # return queryset
-        
