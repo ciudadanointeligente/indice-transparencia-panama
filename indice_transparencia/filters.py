49b854eb37d7d3a0127979e0402730bd65828494
Jordi Bari <jbari@votainteligente.cl>
2019-02-05 21:06:20 +0000

    fix filter in ranking template

--- indice_transparencia/filters.py
+++ indice_transparencia/filters.py
@@ -8,14 +8,14 @@ class PersonFilter(django_filters.FilterSet):
         ('desc','de mayor a menor')
         )
 
-    ordering = django_filters.ChoiceFilter(label='Filtrar según puntaje en índice de transparencia', choices=CHOICES, method='filter_by_order')
+    ordering = django_filters.ChoiceFilter(label='Ordenar según puntaje en índice de transparencia', choices=CHOICES, method='filter_by_order')
 
     class Meta:
         model = Person
         fields = {'circuit', 'party', 'is_deputy'}
 
     def filter_by_order(self, queryset, name, value):
-        expression = '-position_in_ranking' if value == 'desc' else 'position_in_ranking'
+        expression = '-ranking_data__position_in_ranking' if value == 'desc' else 'ranking_data__position_in_ranking'
 
         return queryset.order_by(expression)
         # return queryset
