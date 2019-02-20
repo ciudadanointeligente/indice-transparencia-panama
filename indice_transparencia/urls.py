585da23fbbdc2fb60076b360c22595b68e03e31c
Jordi Bari <jbari@votainteligente.cl>
2019-02-20 16:45:50 +0000

    added 2 extra views

--- indice_transparencia/urls.py
+++ indice_transparencia/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     #path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('', IndexView.as_view(), name='index'),
     path('nosotros', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('info', TemplateView.as_view(template_name='info.html'), name='info'),
+    path('about', TemplateView.as_view(template_name='about_index.html'), name='about_index'),
     path('actualizar_candidato/<str:identifier>/', PersonUpdateView.as_view(), name='update-person-data'),
     path('profile/<str:slug>/', CandidateProfileView.as_view(), name='candidate-profile'),
     path('ranking', RankingListView.as_view(), name='ranking'),
